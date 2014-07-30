4076ae177ea56474a4da9c5761522b1c47673baf
Pierre Poissinger <Pierre.Poissinger@ordina.be>
2014-07-30 15:47:53 +0200

    fMake sonar happy: Util => Private constructor

--- src/main/java/be/ordina/utdemo/mocktype/App.java
+++ src/main/java/be/ordina/utdemo/mocktype/App.java
@@ -2,6 +2,9 @@ package be.ordina.utdemo.mocktype;
 
 public class App {
 
+    private App() {
+    }
+
     public static void main(final String[] args) {
         Car c = new Car(new DieselEngine());
         c.start();
